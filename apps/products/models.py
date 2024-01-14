12c6804db7fab35e50fc1b86219b750f041ae5e5
MahsanGilani <mahsan_gillani@yahoo.com>
2024-01-14 20:28:49 +0330

    some options added to category model

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+from apps.core.models import BaseModel
 
 
-class Category(models.Model):
+class Category(BaseModel):
+    # data fields
     category_name = models.CharField(max_length=100, verbose_name=_('Category Name'))
     category_image = models.ImageField(verbose_name=_('Category image'), upload_to='categories_img/', height_field=None,
                                        width_field=None)
+    slug = models.SlugField(max_length=250, unique=True, blank=True)
+
+    # relations
     sub_category = models.ForeignKey('self', verbose_name=_('Sub Categories'), on_delete=models.CASCADE,
                                      related_name='sub_categories')
-    slug = models.SlugField(max_length=250, unique=True)
 
     class Meta:
-        ordering = ('category_name',)
+        ordering = ('category_name',)  # It does not apply anything to the database
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
         # indexes, improves the speed of searches and filters related to that model in the database.
