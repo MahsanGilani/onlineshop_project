f93eb89c5ec6c74161e595f11b5710e5fb0889a1
MahsanGilani <mahsan_gillani@yahoo.com>
2024-01-14 16:46:24 +0330

    new verbose name added to category model

--- apps/products/models.py
+++ apps/products/models.py
@@ -4,10 +4,11 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Category(models.Model):
-    category_name = models.CharField(max_length=100)
-    category_image = models.ImageField(verbose_name=_('category_image'), upload_to='categories_img/', height_field=None,
+    category_name = models.CharField(max_length=100, verbose_name=_('Category Name'))
+    category_image = models.ImageField(verbose_name=_('Category image'), upload_to='categories_img/', height_field=None,
                                        width_field=None)
-    sub_category_id = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub_categories')
+    sub_category = models.ForeignKey('self', verbose_name=_('Sub Categories'), on_delete=models.CASCADE,
+                                     related_name='sub_categories')
     slug = models.SlugField(max_length=250, unique=True)
 
     class Meta:
