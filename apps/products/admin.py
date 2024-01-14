e2bb8ff7202682a90b8de449f1e97707e46fe018
MahsanGilani <mahsan_gillani@yahoo.com>
2024-01-14 20:29:21 +0330

    category model registered in admin panel

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name', 'created_at', 'updated_at', 'deleted_at')
+    ordering = ('id',)
+    list_filter = ('created_at', 'updated_at', 'deleted_at')
+    search_fields = ('category_name', )
+    list_display_links = ('category_name', )
