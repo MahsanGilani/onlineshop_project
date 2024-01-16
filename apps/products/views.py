f4ff618b34b08549a353eb07c40dbea83fd8354b
MahsanGilani <mahsan_gillani@yahoo.com>
2024-01-16 21:17:26 +0330

    simple view write for urls

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+def index(request):
+    pass
+
+
+def products_list(request):
+    products = Product.actives.all()
+    context = {
+        'products': products
+    }
+    return render(request, '', context)
+
+
+def product_detail(request, pk):
+    pass
