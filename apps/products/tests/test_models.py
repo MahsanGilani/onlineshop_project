b058c1f99c69ca6d0e7cac2282a2fbcb0eb3385f
MahsanGilani <mahsan_gillani@yahoo.com>
2024-01-17 22:48:44 +0330

    product model test added

--- apps/products/tests/test_models.py
+++ apps/products/tests/test_models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 import datetime
 from django.utils.text import slugify
 
-from apps.products.models import Category
+from apps.products.models import Category, Product
 
 class CategoryModelTest(TestCase):
     
@@ -20,59 +20,27 @@ class CategoryModelTest(TestCase):
     def test_parent_category(self):
         self.assertEqual(self.category2.sub_category, self.category1)
 
-    def test_slug_generation(self):
+    def test_slug_cat_generation(self):
         self.assertEqual(self.category3.slug, slugify(self.category3))
 
 
 
-# class ProductModelTest(TestCase):
+class ProductModelTest(TestCase):
 
-#     def setUp(self):
-#         self.category = Category.objects.create(
-#             title='TestCategory',
-#         )
-
-#     def test_product_creation(self):
-#         product = Product.objects.create(
-#             name='TestProduct',
-#             short_description='Test short description',
-#             price=100.00,
-#             category=self.category
-#         )
-#         self.assertEqual(product.name, 'TestProduct')
-#         self.assertEqual(str(product), 'TestProduct')
-
-#     def test_discounted_price(self):
-#         product = Product.objects.create(
-#             name='TestProduct',
-#             short_description='Test short description',
-#             price=100.00,
-#             discount_type='p',
-#             discount_amount=10,
-#             category=self.category
-#         )
-#         self.assertEqual(product.discounted_price, 90)
-
-
-# class ImageModelTest(TestCase):
-
-#     def setUp(self):
-#         self.category = Category.objects.create(
-#             title='TestCategory',
-#         )
-#         self.product = Product.objects.create(
-#             name='TestProduct',
-#             short_description='Test short description',
-#             price=100.00,
-#             category=self.category
-#         )
-
-#     def test_image_creation(self):
-#         image_file = SimpleUploadedFile("test_image.jpg", b"test_content", content_type="image/jpeg")
-#         image = Image.objects.create(
-#             product=self.product,
-#             image=image_file
-#         )
-#         self.assertEqual(str(image), ' تصویرTestProduct')
-
-
+    def setUp(self) -> None:
+        self.product1 = Product(product_name='steak', product_price=800000, product_quantity=5,is_active=True, description='steak is good',slug='steak')
+        self.product2 = Product(product_name='shrimp', product_price=900000, product_quantity=20,is_active=True, description='Shrimp is good',slug='shrimp')                                  
+        self.product3 = Product(product_name='hot dog', product_price=225000, product_quantity=24,is_active=True, description='Hot Dog is good',slug='hot-dog')   
+        self.product4 = Product(product_name='taco', product_price=250000, product_quantity=13,is_active=True, description='Taco is good',slug='taco')
+                                               
+    def test_product_creation(self):
+        self.assertEqual(str(self.product1), f'{self.product1.product_name}')
+        self.assertEqual(str(self.product2), f'{self.product2.product_name}')
+        self.assertEqual(str(self.product3), f'{self.product3.product_name}')
+        self.assertEqual(str(self.product4), f'{self.product4.product_name}')
+        
+    def test_slug_pro_generation(self):
+        self.assertEqual(self.product1.slug, slugify(self.product1))
+        self.assertEqual(self.product2.slug, slugify(self.product2))
+        self.assertEqual(self.product3.slug, slugify(self.product3))
+        self.assertEqual(self.product4.slug, slugify(self.product4))
