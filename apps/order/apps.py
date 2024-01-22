bb5f69b126ba04b553521a8bef53fd3017dd5c52
MahsanGilani <mahsan_gillani@yahoo.com>
2024-01-22 12:50:52 +0330

    verbose name add for all apps

--- apps/order/apps.py
+++ apps/order/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class OrderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.order'
+    verbose_name = _('Order Models List')
--- apps/products/apps.py
+++ apps/products/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.products'
+    verbose_name = _('Product Models List')
