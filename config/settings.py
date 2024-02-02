b09126267feec790e73c6bafc86dcf23a297b6b5
MahsanGilani <mahsan_gillani@yahoo.com>
2024-02-02 21:28:07 +0330

    media urls setup added to config and urls

--- config/settings.py
+++ config/settings.py
@@ -137,10 +137,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-AUTH_USER_MODEL = 'accounts.CustomerUser'
+AUTH_USER_MODEL = 'apps.accounts.CustomerUser'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
